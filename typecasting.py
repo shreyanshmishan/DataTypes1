7d22bb1f8332c2c2d85e4e7f6e4a01ebbe6dd442
shreyanshmishan <shreyanshm.ishan@gmail.com>
2026-06-29 14:37:20 -0400

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+name="shreyansh"
+age=11
+is_student=True
+weight=64.5
+print("name:",name)
+print("data type of name is", type(name))
+print("is_student:", is_student)
+print("data type of is_student is", type(is_student))
+print("age:", age)
+print("data type of age is", type(age))
+print("weight",weight)
+print("data type of weight is", type(weight))
+age=str(age)
+print(age)
+print("data type of age is", type(age))
+weight=int(weight)
+print(weight)
+print("data type of weight is", type(weight))
+is_student=str(is_student)
+print(is_student)
+print("data type of is_student is", type(is_student))
