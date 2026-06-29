c72af647d442d17abc5a63e1411437ba2d75bc29
shreyanshmishan <shreyanshm.ishan@gmail.com>
2026-06-29 14:17:53 -0400

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+a=7
+print("data type of a is", type(a))
+b=4.5
+print("the data type of b is", type(b))
+c=True
+print("the data type of c is", type(c))
+d="shreyansh"
+print("the data type of d is", type(d))
